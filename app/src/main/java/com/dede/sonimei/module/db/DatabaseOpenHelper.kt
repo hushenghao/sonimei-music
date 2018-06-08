ce43e29ca98245a28480ea99a2a9c583184f8f1d
胡晟昊 <863116814@qq.com>
2018-06-08 17:55:58 +0800

    add anko db

---
+++ app/src/main/java/com/dede/sonimei/module/db/DatabaseOpenHelper.kt
@@ -0,0 +1,46 @@
+package com.dede.sonimei.module.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+/**
+ * Created by hsh on 2018/6/6.
+ */
+class DatabaseOpenHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "sonimei_db", null, 1) {
+    companion object {
+        private var instance: DatabaseOpenHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): DatabaseOpenHelper {
+            if (instance == null) {
+                instance = DatabaseOpenHelper(ctx.applicationContext)
+            }
+            return instance!!
+        }
+
+        const val TABLE_SEARCH_HIS = "SearchHis"
+        const val COLUMNS_ID = "_id"
+        const val COLUMNS_TEXT = "text"
+        const val COLUMNS_TIMESTAMP = "time_stamp"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(TABLE_SEARCH_HIS, true,
+                COLUMNS_ID to INTEGER + PRIMARY_KEY + UNIQUE,
+                COLUMNS_TEXT to TEXT,
+                COLUMNS_TIMESTAMP to SqlType.create("TimeStamp NOT NULL DEFAULT (datetime('now','localtime'))")
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Here you can upgrade tables, as usual
+//        db.dropTable("User", true)
+    }
+}
+
+// Access property for Context
+val Context.database: DatabaseOpenHelper
+    get() = DatabaseOpenHelper.getInstance(applicationContext)
+val Context.db: SQLiteDatabase
+    get() = database.writableDatabase
